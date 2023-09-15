dad7ec0c11d34e178a07c5338173ea741956311e
arvindpndit <arvindpandit9808@gmail.com>
2023-09-15 11:42:39 +0530

    added tweet services with hashtag processing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const connect = require("./config/database");
+const { HashtagRepository } = require("./repository/index");
+const TweetService = require("./services/tweet-service");
 
 const app = express();
 
@@ -13,4 +15,28 @@ app.listen(3000, async () => {
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
     }
+
+    let repo = new HashtagRepository();
+    let service = new TweetService();
+    // const tweet = await service.createTweet({
+    //     content: "this is #nodejs  n1ew",
+    // });
+    console.log(tweet);
+    // const tag = await repo.findByName(["Frontend", "Dev"]);
+    // console.log(tag);
+
+    // await repo.bulkCreate([
+    //     {
+    //         title: "dev",
+    //         tweets: [],
+    //     },
+    //     {
+    //         title: "nodejs",
+    //         tweets: [],
+    //     },
+    //     {
+    //         title: "frontend",
+    //         tweets: [],
+    //     },
+    // ]);
 });
